f8995c3c43b552ec2d367bf923611032aaa5fc79
sangil <berk@hansang-il-ui-MacBookPro.local>
2019-05-09 20:18:54 +0900

    added interfaceProduct code

---
+++ contracts/interface/interfaceInvestModule.sol
@@ -0,0 +1,16 @@
+pragma solidity >=0.4.24;
+
+import "./InterfaceModule.sol";
+
+/**
+* @dev 기초 투자 인터페이스 ( 투자자를 위한 )
+*
+* 
+* 
+* 
+ */
+
+contract InterfaceInvestModule is InterfaceModule {
+    
+    uint256 public klay_granularity;
+}
---
+++ contracts/interface/interfacePurchaseModule.sol
@@ -0,0 +1,16 @@
+pragma solidity >=0.4.24;
+
+import "./InterfaceModule.sol";
+
+/**
+* @dev 기초 구매 인터페이스 ( 추후 구매자를 위한 )
+*
+* 
+* 
+* 
+ */
+
+contract InterfacePurchaseModule is InterfaceModule {
+    
+    uint256 public klay_granularity;
+}
